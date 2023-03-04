d0db9f7d8e9146b6942c6794eb118ac72149eac8
Adithya Nethmini <91539930+adithya-nethmini@users.noreply.github.com>
2023-03-05 04:34:47 +0530

    activity

---
+++ app/src/main/java/com/example/clubsync/ClubProfile3Activity.java
@@ -0,0 +1,14 @@
+package com.example.clubsync;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClubProfile3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_club_profile3);
+    }
+}
--- app/src/main/java/com/example/clubsync/MainActivity.java
+++ app/src/main/java/com/example/clubsync/MainActivity.java
@@ -13,17 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.result);
+        button = (Button) findViewById(R.id.signin);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openResultPage();
+
+                openSignInPage();
+            }
+        });
+
+        button = (Button) findViewById(R.id.signup);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUpPage();
             }
         });
     }
 
-    public  void openResultPage(){
-        Intent intent = new Intent(this, HomeActivity.class);
+    public  void openSignInPage(){
+        Intent intent = new Intent(this, SigninActivity.class);
+        startActivity(intent);
+    }
+    public  void openSignUpPage(){
+        Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
     }
 }
