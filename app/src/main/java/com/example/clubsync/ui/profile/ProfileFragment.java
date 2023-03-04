c67bc36c7d114155c88f25eb4ac2d7b7564d52ff
Adithya Nethmini <91539930+adithya-nethmini@users.noreply.github.com>
2023-03-05 04:35:41 +0530

    home and profile

--- app/src/main/java/com/example/clubsync/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/clubsync/ui/profile/ProfileFragment.java
@@ -1,20 +1,25 @@
 package com.example.clubsync.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.clubsync.ClubProfile1Activity;
+import com.example.clubsync.R;
 import com.example.clubsync.databinding.FragmentProfileBinding;
-import com.example.clubsync.databinding.FragmentProfileBinding;
+import com.example.clubsync.ui.profile.ProfileViewModel;
 
 public class ProfileFragment extends Fragment {
 
+    private Button button;
     private FragmentProfileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -25,9 +30,10 @@ public class ProfileFragment extends Fragment {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textProfile;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.profileName;
+//        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
     }
 
     @Override
